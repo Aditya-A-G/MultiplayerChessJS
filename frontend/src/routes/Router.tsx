664a563f86a05c38daa4d506b355dfdb4e5b5f4d
Aditya Ghadge <adityaghadge360@gmail.com>
2024-04-16 02:05:33 +0530

    fix: add vercel config

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -9,44 +9,41 @@ import ProtectedRoute from './ProtectedRoute';
 import { AuthProvider } from '@/contexts/AuthContext';
 
 function Router() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <App />,
-        errorElement: <NotFound />,
-      },
-      {
-        path: '/dashboard',
-        element: (
-          <AuthProvider>
-            <ProtectedRoute element={<Dashboard />} />
-          </AuthProvider>
-        ),
-      },
-      {
-        path: '/games/:gameId',
-        element: (
-          <AuthProvider>
-            <ProtectedRoute element={<Game />} />
-          </AuthProvider>
-        ),
-      },
-      {
-        path: '/signup',
-        element: <SignUp />,
-      },
-      {
-        path: '/login',
-        element: (
-          <AuthProvider>
-            <Login />
-          </AuthProvider>
-        ),
-      },
-    ],
-    { basename: process.env.VERCEL_URL }
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: '/dashboard',
+      element: (
+        <AuthProvider>
+          <ProtectedRoute element={<Dashboard />} />
+        </AuthProvider>
+      ),
+    },
+    {
+      path: '/games/:gameId',
+      element: (
+        <AuthProvider>
+          <ProtectedRoute element={<Game />} />
+        </AuthProvider>
+      ),
+    },
+    {
+      path: '/signup',
+      element: <SignUp />,
+    },
+    {
+      path: '/login',
+      element: (
+        <AuthProvider>
+          <Login />
+        </AuthProvider>
+      ),
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 }
