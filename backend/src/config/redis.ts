a8f3d2d705704cf909e08446aa6c4cdead48b59d
Aditya Ghadge <adityaghadge360@gmail.com>
2024-04-19 01:46:50 +0530

    fix: update redis config

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,85 +1,93 @@
-import { createClient } from 'redis';
+import { RedisClientType, createClient } from 'redis';
 import {
   REDIS_USERNAME,
   REDIS_PASSWORD,
   REDIS_HOST,
   REDIS_PORT,
 } from './config';
+
 import { handleGameEnd } from '../api/sockets/gameHandler';
-
-const redisClient = createClient({
-  username: REDIS_USERNAME,
-  password: REDIS_PASSWORD,
-  socket: {
-    host: REDIS_HOST,
-    port: Number(REDIS_PORT),
-  },
-});
-
-const publisher = redisClient;
-
-redisClient.connect().catch(console.error);
-
-const subscriber = redisClient.duplicate();
-
-subscriber.connect().catch(console.error);
 
 function getUserIdFromKey(key: string): string | null {
   const match = key.match(/^leftTheGame:(.*)$/);
   return match ? match[1] : null;
 }
 
-redisClient.configSet('notify-keyspace-events', 'Ex');
-subscriber.subscribe('__keyevent@0__:expired', async (message) => {
-  const expiredKey = message;
+let redisClient: RedisClientType;
+let publisher: RedisClientType;
+let subscriber: RedisClientType;
+(async (): Promise<any> => {
+  redisClient = createClient({
+    username: REDIS_USERNAME,
+    password: REDIS_PASSWORD,
+    socket: {
+      host: REDIS_HOST,
+      port: Number(REDIS_PORT),
+    },
+  });
 
-  // Acquire lock before performing action
-  const lockKey = `lock:${expiredKey}`;
-  const lockValue = `${process.pid}:${Date.now()}`;
-  const lockAcquired = await redisClient.set(lockKey, lockValue, {
-    NX: true,
-    EX: 10,
-  }); // 10 seconds lock expiration
+  publisher = redisClient;
 
-  if (lockAcquired === 'OK') {
-    try {
-      const playerLeftUserId = getUserIdFromKey(message) as string;
-      let remainingPlayerUserId;
-      const onGoingGameOfPlayer = (await redisClient.hGet(
-        'userGames',
-        playerLeftUserId
-      )) as string;
+  try {
+    await redisClient.connect();
+    subscriber = redisClient.duplicate();
+    await subscriber.connect();
+    redisClient.CONFIG_SET('notify-keyspace-events', 'Ex');
+    subscriber.subscribe('__keyevent@0__:expired', async (message) => {
+      const expiredKey = message;
 
-      const players = await redisClient.sMembers(
-        `games:${onGoingGameOfPlayer}:users`
-      );
+      // Acquire lock before performing action
+      const lockKey = `lock:${expiredKey}`;
+      const lockValue = `${process.pid}:${Date.now()}`;
+      const lockAcquired = await redisClient.set(lockKey, lockValue, {
+        NX: true,
+        EX: 10,
+      }); // 10 seconds lock expiration
 
-      if (players[0] === playerLeftUserId) {
-        remainingPlayerUserId = players[1];
-      } else {
-        remainingPlayerUserId = players[0];
+      if (lockAcquired === 'OK') {
+        try {
+          const playerLeftUserId = getUserIdFromKey(message) as string;
+          let remainingPlayerUserId;
+          const onGoingGameOfPlayer = (await redisClient.hGet(
+            'userGames',
+            playerLeftUserId
+          )) as string;
+
+          const players = await redisClient.sMembers(
+            `games:${onGoingGameOfPlayer}:users`
+          );
+
+          if (players[0] === playerLeftUserId) {
+            remainingPlayerUserId = players[1];
+          } else {
+            remainingPlayerUserId = players[0];
+          }
+
+          const opponentLeftPayload = {
+            method: 'opponentLeft',
+            remainingPlayerUserId: remainingPlayerUserId,
+            playerLeftUserId: playerLeftUserId,
+          };
+
+          publisher.publish(
+            `games:${onGoingGameOfPlayer}`,
+            JSON.stringify(opponentLeftPayload)
+          );
+
+          handleGameEnd(playerLeftUserId, {
+            method: 'endGame',
+            data: { gameId: onGoingGameOfPlayer, reason: 'userLeftTheGame' },
+          });
+        } finally {
+          // Release the lock
+          await redisClient.DEL(lockKey);
+        }
       }
-
-      const opponentLeftPayload = {
-        method: 'opponentLeft',
-        remainingPlayerUserId: remainingPlayerUserId,
-        playerLeftUserId: playerLeftUserId,
-      };
-
-      publisher.publish(
-        `games:${onGoingGameOfPlayer}`,
-        JSON.stringify(opponentLeftPayload)
-      );
-
-      handleGameEnd(playerLeftUserId, {
-        method: 'endGame',
-        data: { gameId: onGoingGameOfPlayer, reason: 'userLeftTheGame' },
-      });
-    } finally {
-      // Release the lock
-      await redisClient.DEL(lockKey);
-    }
+    });
+    return { redisClient, publisher, subscriber };
+  } catch (error) {
+    console.error('Error :', error);
   }
-});
+})();
 
 export { redisClient, publisher, subscriber };
