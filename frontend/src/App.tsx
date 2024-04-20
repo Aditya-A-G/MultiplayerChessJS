042bdee8464448c1d58a24c28bac018be8caf265
Aditya Ghadge <adityaghadge360@gmail.com>
2024-04-20 07:04:19 +0530

    fix: redirect user to dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function App() {
   const navigate = useNavigate();
-
-  navigate('/dashboard');
+  useEffect(() => {
+    navigate('/dashboard');
+  });
   return <h1>Hello World</h1>;
 }
 
